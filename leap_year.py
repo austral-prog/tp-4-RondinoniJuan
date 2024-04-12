ef99481f5066e11e5dcb4a4686d7abd25359c87f
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-12 11:56:42 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,7 +4,6 @@ def leap_year():
     if año % 4 == 0 and year % 100 == 0 :
         if año % 400 == 0 :
             print (f"El año {año} es bisiesto")
-        
         else:
             print (f"El año {año} no es bisiesto")
     else:
