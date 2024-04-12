fda91b3c453386db933da54de907ba307cbc0c0b
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-12 11:51:47 -0300

    Update line.py

--- line.py
+++ line.py
@@ -7,17 +7,14 @@ def line():
     X2 = float(input (""))
     Y2 = float(((A * X2) + B))
 
-    print ("El Coeficiente A de su ecuacion de la recta es: " + A)
-    print ("El Coeficiente B de su ecuacion de la recta es: " + B)
-    print ("El Coeficiente X1 de su ecuacion de la recta es: " + X1)
-    print ("El Coeficiente X2 de su ecuacion de la recta es: " + X2)
-
-    print ("Para la siguiente ecuacion: ")
-    print (f" Y = {A} * X + {B}")
+    print (f"El Coeficiente A de su ecuacion de la recta es: {A}")
+    print (f"El Coeficiente B de su ecuacion de la recta es: {B}")
+    print (f"El Coeficiente X1 de su ecuacion de la recta es: {X1}")
+    print (f"El Coeficiente X2 de su ecuacion de la recta es: {X2}\n")\ 
+    print (f"Para la siguiente ecuacion: ")
+    print (f"\t Y = {A}X + {B}\n")
 
     print ("Dados los siguientes puntos:")
-    P1 = (X1 + Y1)
-    print (P1)
-    P2 = (X2 + Y2)
-    print (P2)
-    print ("La distancia entre ellos es: {math.sqrt((X1 - X2)**2 + (Y1 - Y2)**2)}")
+    print (f"\tP1 ({X1}, {Y1})")
+    print (f"\tP2 ({X2}, {Y2})\n")
+    print (f"La distancia entre ellos es: {math.sqrt((X1 - X2)**2 + (Y1 - Y2)**2)}")
