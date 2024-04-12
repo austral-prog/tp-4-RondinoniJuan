834cc02e5c7ba6a0a50bca9776b0922670090053
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-12 11:34:17 -0300

    Tp4 "Leap year" entregado

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    año = input("")
+    if año % 4 == 0 and year % 100 == 0:
+        if año % 400 == 0:
+            print (f"El año {año} es bisiesto")
+        
+        else:
+            print (f"El año {año} no es bisiesto")
+    else:
+        print (f"El año {año} no es bisiesto")
+    
+    
